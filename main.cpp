9a88d7ba0b8646f565c0e407e8dde7b90f5695ed
Turupawn <ahmed.hn.43@gmail.com>
2017-03-13 17:50:21 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "Test.h"
+#include <iostream>
+using namespace std;
+
+int* getShortestPath(int** graph, int size, int origin)
+{
+    return NULL;
+}
+
+int main ()
+{
+    test();
+    return 0;
+}
